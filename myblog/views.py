379b5262e3226c700f0e3373a6d671815db44b7c
Top10-G <omasphilip@gmail.com>
2025-10-13 23:15:51 +0100

    cleaned up myblog/views.py

--- myblog/views.py
+++ myblog/views.py
@@ -20,27 +20,3 @@ def signup(request):
             messages.error(request, "Please correct the errors below.")
 
     return render(request, 'signup.html', {'signupform': form})
-
-
-
-
-
-
-
-# from django.shortcuts import render, redirect
-# from django.contrib import messages
-# from post.forms import SignUpForm
-
-# def signup(request):
-#     if request.method == "POST":
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, "Account created successfully! You can now log in.")
-#             return redirect('login')
-#         else:
-#             messages.error(request, "Please correct the errors below.")
-#     else:
-#         form = SignUpForm()
-
-#     return render(request, 'signup.html', {'signupform': form})
